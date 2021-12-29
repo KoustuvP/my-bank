3a606505b8075de37c382dd15f1386f0bda0d861
KoustuvP <kmukherjee564@gmail.com>
2021-12-29 18:15:52 +0530

    3 files modified, 3 added

--- src/app/mortgage/mortgage-routing.module.ts
+++ src/app/mortgage/mortgage-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HowToApplyComponent } from './how-to-apply/how-to-apply.component';
+import { MortgageOptionsComponent } from './mortgage-options/mortgage-options.component';
 import { MortgageComponent } from './mortgage.component';
 const routes: Routes = [
-  {path:'', redirectTo: 'how-to-apply', pathMatch:'full'},
-  {path:'how-to-apply', component: HowToApplyComponent}
-
+  { path: '', redirectTo: 'how-to-apply', pathMatch: 'full' },
+  { path: 'how-to-apply', component: HowToApplyComponent },
+  { path: 'mortgage-options', component: MortgageOptionsComponent },
 ];
 
 @NgModule({
--- src/app/mortgage/mortgage.module.ts
+++ src/app/mortgage/mortgage.module.ts
@@ -1,12 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
 import { SharedModule } from '../shared/shared.module';
 import { HowToApplyComponent } from './how-to-apply/how-to-apply.component';
+import { MortgageOptionsComponent } from './mortgage-options/mortgage-options.component';
 import { MortgageRoutingModule } from './mortgage-routing.module';
 import { MortgageComponent } from './mortgage.component';
 
 @NgModule({
-  imports: [SharedModule, MortgageRoutingModule],
-  declarations: [MortgageComponent, HowToApplyComponent],
+  imports: [SharedModule, MortgageRoutingModule, CommonModule],
+  declarations: [MortgageComponent, HowToApplyComponent, MortgageOptionsComponent],
 })
 export class MortgageModule {}
