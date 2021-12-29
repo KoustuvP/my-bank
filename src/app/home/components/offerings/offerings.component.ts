ffa1d5751a3f026c04be5942e7a3877bcb0ab8b3
KoustuvP <kmukherjee564@gmail.com>
2021-12-29 14:35:18 +0530

    6 files modified

--- src/app/home/components/offerings/offerings.component.ts
+++ src/app/home/components/offerings/offerings.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-offerings',
   templateUrl: './offerings.component.html',
-  styleUrls: ['./offerings.component.css']
+  styleUrls: ['./offerings.component.css'],
 })
 export class OfferingsComponent implements OnInit {
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: false,
+    touchDrag: false,
+    pullDrag: false,
+    dots: false,
+    navSpeed: 700,
+    navText: ['Previous', 'Next'],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      400: {
+        items: 2,
+      },
+      740: {
+        items: 3,
+      },
+      940: {
+        items: 4,
+      },
+    },
+    nav: true,
+  };
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,9 +2,16 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { OfferingsComponent } from './components/offerings/offerings.component';
 import { HomeRoutingModule } from './home-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CarouselModule } from 'ngx-owl-carousel-o';
 
 @NgModule({
-  imports: [SharedModule, HomeRoutingModule],
+  imports: [
+    SharedModule,
+    HomeRoutingModule,
+    BrowserAnimationsModule,
+    CarouselModule,
+  ],
   declarations: [OfferingsComponent],
 })
 export class HomeModule {}
