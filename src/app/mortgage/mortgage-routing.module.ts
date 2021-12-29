e16d2bb610472cc1e3801aef8382ca4c02b06e71
KoustuvP <kmukherjee564@gmail.com>
2021-12-29 18:26:31 +0530

    2 files modified, 3 added

--- src/app/mortgage/mortgage-routing.module.ts
+++ src/app/mortgage/mortgage-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { HowToApplyComponent } from './how-to-apply/how-to-apply.component';
 import { MortgageOptionsComponent } from './mortgage-options/mortgage-options.component';
 import { MortgageComponent } from './mortgage.component';
+import { PropertyDetailsComponent } from './property-details/property-details.component';
 const routes: Routes = [
   { path: '', redirectTo: 'how-to-apply', pathMatch: 'full' },
   { path: 'how-to-apply', component: HowToApplyComponent },
   { path: 'mortgage-options', component: MortgageOptionsComponent },
+  { path: 'property-details', component: PropertyDetailsComponent },
 ];
 
 @NgModule({
--- src/app/mortgage/mortgage.module.ts
+++ src/app/mortgage/mortgage.module.ts
@@ -5,9 +5,10 @@ import { HowToApplyComponent } from './how-to-apply/how-to-apply.component';
 import { MortgageOptionsComponent } from './mortgage-options/mortgage-options.component';
 import { MortgageRoutingModule } from './mortgage-routing.module';
 import { MortgageComponent } from './mortgage.component';
+import { PropertyDetailsComponent } from './property-details/property-details.component';
 
 @NgModule({
   imports: [SharedModule, MortgageRoutingModule, CommonModule],
-  declarations: [MortgageComponent, HowToApplyComponent, MortgageOptionsComponent],
+  declarations: [MortgageComponent, HowToApplyComponent, MortgageOptionsComponent,PropertyDetailsComponent],
 })
 export class MortgageModule {}
