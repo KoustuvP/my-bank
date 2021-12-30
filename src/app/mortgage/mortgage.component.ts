55051e8193c808b86a77c9e818cbed5c686afb94
KoustuvP <kmukherjee564@gmail.com>
2021-12-30 18:29:18 +0530

    3 files modified, 5 added

--- src/app/mortgage/mortgage.component.ts
+++ src/app/mortgage/mortgage.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { NavigationCancel, NavigationEnd, NavigationStart, Router } from '@angular/router';
+import { catchError, delay, forkJoin, map, of } from 'rxjs';
+import { LoaderService } from '../shared/services/loader.service';
+import { MortgageService } from './services/mortgage.service';
 
 @Component({
   selector: 'app-mortgage',
@@ -20,50 +23,51 @@ export class MortgageComponent implements OnInit {
   isOpened: boolean = false;
 
   constructor(
-    private router: Router //  private loaderService: LoaderService,
-  ) //  private mortgageService: MortgageService
+    private router: Router, private loaderService: LoaderService,
+    private mortgageService: MortgageService
+  )   
   {}
 
   ngOnInit(): void {
-    // this.router.events.pipe(map(event => {
-    //   if (event instanceof NavigationStart) {
-    //     this.loaderService.changeLoadingState(true)
-    //   }
-    //   return event;
-    // }),
-    //   delay(1000)).subscribe(event => {
-    //     if (event instanceof NavigationEnd) {
-    //       this.loaderService.changeLoadingState(false)
-    //     }
-    //     if (event instanceof NavigationCancel) {
-    //       this.loaderService.changeLoadingState(false)
-    //       this.isOpened = true;
-    //     }
-    //   })
-    // forkJoin({
-    //   propertyDetails: this.mortgageService.getPropertyDetails().pipe(catchError(err=>of([]))),
-    //   valuation: this.mortgageService.getValuation().pipe(catchError(err=>of([]))),
-    //   paymentDetails: this.mortgageService.getPaymentDetails().pipe(catchError(err=>of(undefined)))
-    // })
-    //   .subscribe(({ propertyDetails, valuation, paymentDetails }) => {
-    //     if (propertyDetails&&propertyDetails.length) {
-    //       if (propertyDetails.length == valuation.length && paymentDetails) {
-    //         //this.mortgageService.startFresh = true;
-    //         this.mortgageService.setAvailability(true, true, true,true)
-    //       }
-    //       else if (propertyDetails.length == valuation.length && !paymentDetails) {
-    //         this.mortgageService.startFresh = false;
-    //         this.mortgageService.setAvailability(true, true, false,true)
-    //       }
-    //       else if (propertyDetails.length > valuation.length) {
-    //         this.mortgageService.startFresh = false;
-    //         this.mortgageService.setAvailability(true, true, false,false)
-    //       }
-    //     }
-    //     else {
-    //       this.mortgageService.startFresh = false;
-    //       this.mortgageService.setAvailability(false, false, false,false)
-    //     }
-    //   });
+    this.router.events.pipe(map(event => {
+      if (event instanceof NavigationStart) {
+        this.loaderService.changeLoadingState(true)
+      }
+      return event;
+    }),
+      delay(1000)).subscribe(event => {
+        if (event instanceof NavigationEnd) {
+          this.loaderService.changeLoadingState(false)
+        }
+        if (event instanceof NavigationCancel) {
+          this.loaderService.changeLoadingState(false)
+          this.isOpened = true;
+        }
+      })
+    forkJoin({
+      propertyDetails: this.mortgageService.getPropertyDetails().pipe(catchError(err=>of([]))),
+      valuation: this.mortgageService.getValuation().pipe(catchError(err=>of([]))),
+      paymentDetails: this.mortgageService.getPaymentDetails().pipe(catchError(err=>of(undefined)))
+    })
+      .subscribe(({ propertyDetails, valuation, paymentDetails }) => {
+        if (propertyDetails&&propertyDetails.length) {
+          if (propertyDetails.length == valuation.length && paymentDetails) {
+            //this.mortgageService.startFresh = true;
+            this.mortgageService.setAvailability(true, true, true,true)
+          }
+          else if (propertyDetails.length == valuation.length && !paymentDetails) {
+            this.mortgageService.startFresh = false;
+            this.mortgageService.setAvailability(true, true, false,true)
+          }
+          else if (propertyDetails.length > valuation.length) {
+            this.mortgageService.startFresh = false;
+            this.mortgageService.setAvailability(true, true, false,false)
+          }
+        }
+        else {
+          this.mortgageService.startFresh = false;
+          this.mortgageService.setAvailability(false, false, false,false)
+        }
+      });
   }
 }
