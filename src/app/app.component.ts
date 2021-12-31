5a85e808d4e9d69701be9383dcae8a82968ba93b
KoustuvP <kmukherjee564@gmail.com>
2021-12-31 19:50:03 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { CurrentUser } from './shared/models/CurrentUser';
+import { AuthService } from './shared/services/auth.service';
+import { LoaderService } from './shared/services/loader.service';
+import { NotificationService } from './shared/services/notification.service';
 
 @Component({
   selector: 'my-app',
@@ -6,5 +11,116 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  name = 'Angular';
+  title = 'santander-angular';
+  idleState = 'Not started.';
+  showTimeOut = false;
+  timedOut = false;
+  lastPing?: Date;
+  idleEndSubscription: any;
+  idleStartSubscription: any;
+  timeoutSubscription: any;
+  timeoutWarningSubscription: any;
+  keepaliveSubscription: any;
+  routerSubscription: any;
+  isLoading = false;
+  userName='';
+
+  constructor(
+    private idle: Idle,
+    private keepalive: Keepalive,
+    private router: Router,
+    private authService: AuthService,
+    public notificationService: NotificationService,
+    private loaderService: LoaderService,
+    private ref: ChangeDetectorRef
+  ) {
+
+    this.authService.getCurrentUserSubscription().subscribe((user:CurrentUser)=>this.userName=user.firstName?user.firstName+" "+user.lastName:'')
+    idle.setIdle(15);
+    idle.setTimeout(15);
+    idle.setInterrupts(DEFAULT_INTERRUPTSOURCES);
+    keepalive.interval(15);
+    this.reset();
+  }
+  // ngAfterContentInit(): void {
+  //   this.userName=this.authService.getCurrentUser()?.firstName+" "+this.authService.getCurrentUser()?.lastName
+  // }
+  ngOnDestroy(): void {
+    this.idleEndSubscription.unsubscribe();
+    this.idleStartSubscription.unsubscribe();
+    this.timeoutSubscription.unsubscribe();
+    this.timeoutWarningSubscription.unsubscribe();
+    this.keepaliveSubscription.unsubscribe();
+    this.routerSubscription.unsubscribe();
+
+  }
+  ngOnInit(): void {
+    this.loaderService.getLoadingState().subscribe((data: boolean) => this.isLoading = data);
+   
+    // this.router.events.subscribe(event => {
+      
+    //   if (event instanceof NavigationStart)
+    //     this.loaderService.changeLoadingState(true)
+    //   if (event instanceof NavigationEnd)
+    //     this.loaderService.changeLoadingState(false)
+    //   if (event instanceof NavigationCancel){
+    //     this.loaderService.changeLoadingState(false);
+        
+    //   }
+    // })
+    this.idleEndSubscription = this.idle.onIdleEnd.subscribe(() => {
+      // this.idleState = 'No longer idle.'
+      // this.idle.watch(false)
+      // if(this.showTimeOut)
+      // this.idle.clearInterrupts()
+    });
+
+    this.timeoutSubscription = this.idle.onTimeout.subscribe(() => {
+      this.idleState = 'Timed out!';
+      this.timedOut = true;
+      this.showTimeOut = false;
+      this.idle.setInterrupts(DEFAULT_INTERRUPTSOURCES)
+      this.authService.removeUser();
+    });
+
+    this.idleStartSubscription = this.idle.onIdleStart.subscribe(() => {
+      this.idleState = 'You\'ve gone idle!'
+      this.showTimeOut = true;
+    });
+
+    this.timeoutWarningSubscription = this.idle.onTimeoutWarning.subscribe((countdown) => {
+      if (this.showTimeOut)
+        this.idle.clearInterrupts();
+    });
+    this.keepaliveSubscription = this.keepalive.onPing.subscribe(() => this.lastPing = new Date());
+    this.routerSubscription = this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        (event.url == '/login') ? this.idle.stop() : this.idle.watch()
+      }
+    })
+
+  }
+
+  reset() {
+    this.idle.watch();
+    this.idleState = 'Started.';
+    this.timedOut = false;
+    this.showTimeOut = false;
+  }
+
+  onContinue() {
+    this.idle.setInterrupts(DEFAULT_INTERRUPTSOURCES)
+    this.reset();
+  }
+
+  onLoggingOut() {
+    this.idle.stop();
+    this.showTimeOut = false;
+    this.idle.setInterrupts(DEFAULT_INTERRUPTSOURCES)
+    this.authService.removeUser();
+  }
+}
+
+function DEFAULT_INTERRUPTSOURCES(DEFAULT_INTERRUPTSOURCES: any) {
+throw new Error('Function not implemented.');
 }
