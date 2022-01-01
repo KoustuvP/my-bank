df34bbb2d396b8582ee98dfc27b41ab6d08c2b57
KoustuvP <kmukherjee564@gmail.com>
2022-01-01 13:09:46 +0530

    3 files modified

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,13 +5,16 @@ import { HomeRoutingModule } from './home-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 // import { CarouselModule } from 'ngx-owl-carousel-o';
 import { HomeComponent } from './home.component';
+import { CaraouselModule } from '../caraousel/caraousel.module';
 //import { IvyCarouselModule } from "angular-responsive-carousel";
+
 
 @NgModule({
   declarations: [HomeComponent, OfferingsComponent],
   imports: [
     SharedModule,
     HomeRoutingModule,
+    CaraouselModule
   //  CarouselModule, //IvyCarouselModule
   ],
 })
