eb1bb75600caacec11a98ae38bbf94bf7e113fc5
KoustuvP <kmukherjee564@gmail.com>
2022-01-03 15:59:05 +0530

    2 files modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './auth.guard';
-// import { AboutComponent } from './about/about.component';
-// import { AuthGuard } from './auth.guard';
-// import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
-  // { path: 'about', component: AboutComponent, canActivate:[AuthGuard] },
   {
     path: 'home',
     loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
-//import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
-//import { HeaderComponent } from './header/header.component';
-//import { FooterComponent } from './footer/footer.component';
 import { RouterModule } from '@angular/router';
 
 import { MatCardModule } from '@angular/material/card';
